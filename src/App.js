71e820af67c57918dfa71322f1849eed71fb3b35
Matthew Litwin <mlitwin@sonic.net>
2022-10-26 21:39:15 -0400

    More try at main layout css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import './App.css';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import Nav from 'react-bootstrap/Nav';
 
 import ObservationEntry from './ObservationEntry';
 import ObservationHistory from './ObservationHistory';
+import Species from './common/Species';
+
+import {latestObservation} from './store/store'
+
 
 function App() {
 
+  const latest = latestObservation();
+
+  return (
+    <Tab.Container className="AppContainer" defaultActiveKey="first">
+      <Nav className="AppHeader">
+        <Nav.Item>
+          <Nav.Link eventKey="first">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="second">History</Nav.Link>
+        </Nav.Item>
+        <Species species={latest} />
+      </Nav>
+      <Tab.Content className="AppContent">
+            <Tab.Pane eventKey="first">
+              <ObservationEntry />
+            </Tab.Pane>
+            <Tab.Pane eventKey="second">
+              <ObservationHistory />
+            </Tab.Pane>
+          </Tab.Content>
+    </Tab.Container>
+  )
+  /*
   return (
     <div className="App">
       <Tabs className="AppTabs">
@@ -19,6 +48,7 @@ function App() {
       </Tabs>
     </div>
   );
+  */
 }
 
 export default App;
--- src/ObservationEntry.js
+++ src/ObservationEntry.js
@@ -1,7 +1,6 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Keypad from './Keypad';
 import SpeciesPicker from './SpeciesPicker';
-import Species from './common/Species';
 import {checklist, latestObservation} from './store/store'
 import Container from 'react-bootstrap/Container';
 import { Nav } from 'react-bootstrap';
@@ -20,13 +19,9 @@ function ObservationEntry() {
     setActiveState(newActive);
   }
   const ck = checklist();
-  const latest = latestObservation();
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className="ObservationEntryPad">
       <div>
-        <Species species={latest} />
         <SpeciesPicker species={ck} active={active} />
         <Keypad changeActive={changeActive} />
       </div>
