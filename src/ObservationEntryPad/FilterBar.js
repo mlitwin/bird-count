26d0f178fce4cbb8954762f39cda3a48f35cb1c2
Matthew Litwin <mlitwin@sonic.net>
2022-10-29 10:56:45 -0400

    Tweaks

--- src/ObservationEntryPad/FilterBar.js
+++ src/ObservationEntryPad/FilterBar.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { ButtonGroup } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import "./FilterBar.css";
 
 function FilterBar(props) {
-
   return (
     <div className="KeypadControls">
-      <div className="KeyboadInputArea">
-      <Button>+</Button>
-      <div>&#8203;{props.filter}</div>
-      </div>
+      <Button>ENTER</Button>
+      <div className="FilterValue">&#8203;{props.filter}</div>
+      <ButtonGroup className="FilterControls">
+        <Button>BSP</Button>
+        <Button>CLEAR</Button>
+      </ButtonGroup>
     </div>
   );
 }
