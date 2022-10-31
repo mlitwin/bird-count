b8c9145bdf4a17e44fb29cc03d676b40e12f1170
Matthew Litwin <mlitwin@sonic.net>
2022-10-31 09:17:44 -0400

    Caps

--- src/ObservationEntryPad/Keypad.js
+++ src/ObservationEntryPad/Keypad.js
@@ -6,7 +6,7 @@ import "./Keypad.css";
 function Keypad(props) {
   const keyboard = useRef();
   const layout = {
-    default: ["q w e r t y u i o p", "a s d f g h j k l", "z x c v b n m"],
+    default: ["Q W E R T Y U I O P", "A S D F G H J K L", "Z X C V B N M"],
   };
 
   const onKeyPress = (k) => {
