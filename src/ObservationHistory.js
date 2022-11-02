17f9346448a5de6e3bada9decb583dd65178a96d
Matthew Litwin <mlitwin@sonic.net>
2022-11-02 19:47:17 -0400

    Simple export

--- src/ObservationHistory.js
+++ src/ObservationHistory.js
@@ -4,6 +4,8 @@ import { observations, clearObservations } from "./store/store";
 
 function ObservationHistory() {
   const obs = observations();
+  const mail = encodeURIComponent(JSON.stringify(obs, null, 2));
+  const mailto = `mailto:?body=${mail}&subject=Email the List`;
 
   function doClear() {
     clearObservations();
@@ -12,6 +14,7 @@ function ObservationHistory() {
     <div className="oneColumn">
       <Observations observations={obs} />
       <Button onClick={(e) => doClear()}>CLEAR</Button>
+      <a href={mailto}>Email the List</a>
     </div>
   );
 }
