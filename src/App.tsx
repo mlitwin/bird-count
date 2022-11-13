b9dee7026bb1beb4e6945550508368f50c23686e
Matthew Litwin <mlitwin@sonic.net>
2022-11-12 19:35:27 -0800

    Move header to head

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,14 @@ function App() {
 
   return (
     <Tab.Container defaultActiveKey="first">
+            <Nav className="AppHeader">
+        <Nav.Item>
+          <Nav.Link eventKey="first">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="second">History</Nav.Link>
+        </Nav.Item>
+      </Nav>
       <Tab.Content className="AppContent">
         <Tab.Pane eventKey="first">
           <ObservationEntryPad />
@@ -18,14 +26,6 @@ function App() {
           <ObservationHistory />
         </Tab.Pane>
       </Tab.Content>
-      <Nav className="AppHeader">
-        <Nav.Item>
-          <Nav.Link eventKey="first">Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="second">History</Nav.Link>
-        </Nav.Item>
-      </Nav>
     </Tab.Container>
   );
 }
