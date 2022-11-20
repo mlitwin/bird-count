ec8ee5467051b4b18f03f129642756ee13bb49d8
Matthew Litwin <mlitwin@sonic.net>
2022-11-20 13:42:50 -0800

    Missed file

---
+++ src/common/SpeciesName.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function SpeciesName(props) {
+
+    const species = props.species;
+    const speciesName = species ? species.localizations.en.commonName : '';
+
+    return (
+        <span>{speciesName}</span>
+    )
+}
+
+export default SpeciesName;
