f441717f73788acad0f1840db775525c9da5478c
Matthew Litwin <mlitwin@sonic.net>
2022-12-09 18:08:41 -0800

    cleanup

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { bind } from "@react-rxjs/core";
 import { createSignal } from "@react-rxjs/utils";
 import {
