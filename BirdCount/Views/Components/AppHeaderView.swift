2c3b18c5fdc8f1a6ff6cd2a0923a1cefd82b63f3
Matthew Litwin <mlitwin@sonic.net>
2025-09-20 16:16:51 -0700

    tweak fade

--- BirdCount/Views/Components/AppHeaderView.swift
+++ BirdCount/Views/Components/AppHeaderView.swift
@@ -53,10 +53,8 @@ struct AppHeaderView: View {
             // Gradient background that transitions from opaque to transparent
             LinearGradient(
                 gradient: Gradient(stops: [
-                    .init(color: Color(.systemGroupedBackground), location: 0.0),
-                    .init(color: Color(.systemGroupedBackground), location: 0.6),
-                    .init(color: Color(.systemGroupedBackground).opacity(0.8), location: 0.8),
-                    .init(color: Color(.systemGroupedBackground).opacity(0.0), location: 1.0)
+                    .init(color: Color(.systemGroupedBackground), location: 0.95),
+                    .init(color: Color(.systemGroupedBackground).opacity(0.5), location: 1.0)
                 ]),
                 startPoint: .top,
                 endPoint: .bottom
